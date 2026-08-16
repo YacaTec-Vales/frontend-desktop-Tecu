d3364457dca2e9d8e124042e817fb695767467ad
y0ADQ1 <juanderasduron@gmail.com>
2026-08-16 04:40:56 -0600

    fix(staff): extraer propiedad data de la respuesta del envelope

--- src/app/core/services/staff.service.ts
+++ src/app/core/services/staff.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { Coordinador, Verificador, Cajero, CreateStaffDto } from '../models/staff.model';
 
@@ -14,28 +14,40 @@ export class StaffService {
 
   // Coordinadores
   getCoordinadores(): Observable<Coordinador[]> {
-    return this.http.get<Coordinador[]>(`${this.baseUrl}/coordinadores`);
+    return this.http.get<{message: string, data: Coordinador[]}>(`${this.baseUrl}/coordinadores`).pipe(
+      map(res => res.data)
+    );
   }
 
   createCoordinador(data: CreateStaffDto): Observable<Coordinador> {
-    return this.http.post<Coordinador>(`${this.baseUrl}/coordinadores`, data);
+    return this.http.post<{message: string, data: Coordinador}>(`${this.baseUrl}/coordinadores`, data).pipe(
+      map(res => res.data)
+    );
   }
 
   // Verificadores
   getVerificadores(): Observable<Verificador[]> {
-    return this.http.get<Verificador[]>(`${this.baseUrl}/verificadores`);
+    return this.http.get<{message: string, data: Verificador[]}>(`${this.baseUrl}/verificadores`).pipe(
+      map(res => res.data)
+    );
   }
 
   createVerificador(data: CreateStaffDto): Observable<Verificador> {
-    return this.http.post<Verificador>(`${this.baseUrl}/verificadores`, data);
+    return this.http.post<{message: string, data: Verificador}>(`${this.baseUrl}/verificadores`, data).pipe(
+      map(res => res.data)
+    );
   }
 
   // Cajeros
   getCajeros(): Observable<Cajero[]> {
-    return this.http.get<Cajero[]>(`${this.baseUrl}/cajeros`);
+    return this.http.get<{message: string, data: Cajero[]}>(`${this.baseUrl}/cajeros`).pipe(
+      map(res => res.data)
+    );
   }
 
   createCajero(data: CreateStaffDto): Observable<Cajero> {
-    return this.http.post<Cajero>(`${this.baseUrl}/cajeros`, data);
+    return this.http.post<{message: string, data: Cajero}>(`${this.baseUrl}/cajeros`, data).pipe(
+      map(res => res.data)
+    );
   }
 }
